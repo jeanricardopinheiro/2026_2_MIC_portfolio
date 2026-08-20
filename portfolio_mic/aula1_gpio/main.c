cb7f88c6c06920b580322b0e2bc7d139e36622b9
Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>
2026-08-20 11:38:36 -0300

    Alterações para chaveamento no circuito
    Signed-off-by: Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>

--- portfolio_mic/aula1_gpio/main.c
+++ portfolio_mic/aula1_gpio/main.c
@@ -11,12 +11,15 @@
 int main(void)
 {
 	DDRB |= 0b00100000; //define pino PB5 como saída
+	DDRD &= 0b11110111; //define pino PD3 como entrada
     while(1)
     {
-		PORTB|= 0b00100000; // atribui o nivel lógico alto ao pino PB5
-		_delay_ms(100);
-		PORTB &= 0b11011111; //atribui nivel lógico baixo ao pino PB5
-		_delay_ms(900);
-        //TODO:: Please write your application code 
+		if( (PIND & 0b00001000) !=0) { // ler valor da chave PD3 (operador & isola apenas o pino 3, se estiver alto > 0  continua o código abaixo) 
+			PORTB|= 0b00100000; // atribui o nivel lógico alto ao pino PB5
+			_delay_ms(100);
+			PORTB &= 0b11011111; //atribui nivel lógico baixo ao pino PB5
+			_delay_ms(900);
+			//TODO:: Please write your application code 
+		}
     }
 }
