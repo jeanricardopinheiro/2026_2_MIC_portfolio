8d0857dc8cc2155950928aa73f1d85df45ec7b28
Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>
2026-08-20 11:17:10 -0300

    Esqueleto do projeto e aula 1 Operações bit a bit com circuito básico.
    Signed-off-by: Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>

---
+++ portfolio_mic/aula1_gpio/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ * Created: 8/20/2026 10:37:11 AM
+ *  Author: Jean Ricardo Pinheiro
+ */ 
+#define F_CPU 16000000
+#include <xc.h>
+#include "util/delay.h"
+
+int main(void)
+{
+	DDRB |= 0b00100000; //define pino PB5 como saída
+    while(1)
+    {
+		PORTB|= 0b00100000; // atribui o nivel lógico alto ao pino PB5
+		_delay_ms(100);
+		PORTB &= 0b11011111; //atribui nivel lógico baixo ao pino PB5
+		_delay_ms(900);
+        //TODO:: Please write your application code 
+    }
+}
