d7c5f8b06b82644f1a200a4ed5a68eb4a8f73a31
Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>
2026-08-27 11:34:11 -0300

    Proteção de parte do código contra interrupções Interrupção desabilitada durante o ativo do led.
    Signed-off-by: Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>

--- portfolio_mic/aula3_interrupt/main.c
+++ portfolio_mic/aula3_interrupt/main.c
@@ -13,14 +13,23 @@
 ISR(INT0_vect) //função no vetor de interrupção INT0
 {
 	PORTB |= (1<< PORTB1); // acende led em PB1
+	_delay_ms(50);
+	PORTB &= ~(1<<PORTB1); // apaga led PB1
+}
+
+ISR(INT1_vect) //função no vetor de interrupção INT0
+{
+	PORTB |= (1<< PORTB1); // acende led em PB1
 	_delay_ms(100);
 	PORTB &= ~(1<<PORTB1); // apaga led PB1
 }
 
+
 void INT_config()
 {
-	EICRA = (1<<ISC01) | (0<<ISC00); // CONFIGURA INT0 na borda de descida
-	EIMSK = (1<<INT0); //habilida INT0
+	EICRA = (1<<ISC11) | (0<<ISC10)  // configura INT1 na borda de subida
+		  | (1<<ISC01) | (0<<ISC00); //configura INT0 na borda de descida
+	EIMSK = (1<<INT1) | (1<<INT0); //habilida INT0
 }
 
 void GPIO_config()
@@ -35,7 +44,11 @@ int main(void)
 	sei(); //habilida intrrupções globalmente
 	while(1)
     {
-        _delay_ms(100);
-		PORTB ^= (1<<PORTB0);
+		cli(); //desabilida interrupções temporariamente
+		PORTB ^= (1<<PORTB0); //acende LED no pino PB0
+		_delay_ms(100);
+		sei(); // reativa interrupções
+		PORTB &= ~(1<<PORTB0); // apaga LED no pino PBA
+		_delay_ms(100);
     }
 }
