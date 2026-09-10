c73a1d1b5b1a1ddf5c51d34bd908fc539f553755
Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>
2026-09-10 10:41:43 -0300

    Roteiro Datapool - Parte 2 Código liga os LEDs da bancada Datapool através do nível lógico baixo.
    Signed-off-by: Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>

--- portfolio_mic/aula5_avrdude/main.c
+++ portfolio_mic/aula5_avrdude/main.c
@@ -1,22 +1,43 @@
 /*
  * main.c
  *
- * Created: 9/10/2026 8:36:27 AM
- *  Author: Jean Ricardo Pinheiro
- */ 
+ * Created: 9/10/2026
+ * Author: Jean Ricardo Pinheiro
+ */
 
 #define F_CPU 16000000
 #include <xc.h>
 #include "util/delay.h"
 
-void GPIO_config(){
-	DDRB |= (1<<DDB5); //configura o pino PB5 (13) como saida
+uint8_t barra = 0x00;
+
+void GPIO_incBar() {
+	barra = (barra << 1) | 0b00000001;
+	PORTD = ~barra;
 }
 
-int main(void){
-    GPIO_config();
-	while(1){
-        PORTB ^= (1<<PORTB5); //alterna o estado do LED 
-		_delay_ms(500);
-    }
+void GPIO_decBar() {
+	barra = barra >> 1;
+	PORTD = ~barra;
+}
+
+void GPIO_config() {
+	DDRD = 0xFF;        // pinos usados pelos LEDs como saída
+	PORTD = 0xFF;       // LEDs apagados
+}
+
+int main(void) {
+	GPIO_config();
+
+	while (1) {
+		for (uint8_t i = 0; i < 8; i++) {
+			GPIO_incBar();
+			_delay_ms(200);
+		}
+
+		for (uint8_t i = 0; i < 8; i++) {
+			GPIO_decBar();
+			_delay_ms(200);
+		}
+	}
 }
