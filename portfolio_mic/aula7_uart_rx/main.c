290c82978c53f661f8b100beb2aa10a2799f02bd
Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>
2026-09-24 11:22:33 -0300

    Tratamento de recepção UART via interrupção.
    Signed-off-by: Jean Ricardo Pinheiro <jean.rp@aluno.ifsc.edu.br>

--- portfolio_mic/aula7_uart_rx/main.c
+++ portfolio_mic/aula7_uart_rx/main.c
@@ -7,11 +7,13 @@
 #define  F_CPU 16000000
 #include <xc.h>
 #include "util/delay.h"
+#include "avr/interrupt.h"
 
 /*configuração do módulo USRT0 para o modo assíncrono , frame de 8 bits, pariedade par, BAUND varialvel*/
 void UART_config_rx(uint16_t pBAUND){
 	UCSR0A = (0<<U2X0);							// modo "double speed" desativado
-	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02); //habilita o receptor UART0, frame de 8 bits
+	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02) //habilita o receptor UART0, frame de 8 bits
+			| (1<<RXCIE0); //habilita a interrupção de recebimento da UART
 	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)			//modo assincrono
 	|(1<<UPM01)|(0<<UPM00)			// habilita pariedade par
 	|(1<<USBS0)						//2 BIT DE STOP
@@ -26,19 +28,24 @@ void GPIO_config(){
 	DDRC = (1<<DDC0)|(1<<DDC1); //PC0 e PC1 como saida
 }
 
+
+ISR(USART_RX_vect){
+	uint8_t tReceivedByte = UDR0; //leitura do buffer UART
+	//Tratamendo da mensagem recebida
+	if(tReceivedByte == 'M'){
+		PORTC |= (1<<PORTC0); //led verde
+		}else{
+		PORTC |= (1<<PORTC1); //led vermelho
+	}
+	_delay_ms(1);
+	PORTC = 0; // apaga os leds que estiverem acessos
+}
+
 int main(void){
 	GPIO_config();
 	UART_config_rx(9600);
+	sei(); //habilita interrupções globais
     while(1){
-        while((UCSR0A & (1<<RXC0) ) == 0); //semáforo na flag de recebimento da UART
-		uint8_t tReceivedByte = UDR0; //leitura do buffer UART
-		//Tratamendo da mensagem recebida
-		if(tReceivedByte == 'M'){
-			PORTC |= (1<<PORTC0); //led verde
-		}else{
-			PORTC |= (1<<PORTC1); //led vermelho
-		}
-		_delay_ms(1);
-		PORTC = 0; // apaga os leds que estiverem acessos
+		
     }
 }
